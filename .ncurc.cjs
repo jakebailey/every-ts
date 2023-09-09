2c3cd50f4f97808bd2fc2515ee4bd0494e8abf16
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-09-09 15:06:23 -0700

    Some starter stuff

---
+++ .ncurc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    target: (dependencyName, [{ semver, version, operator, major, minor, patch, release, build }]) => {
+        if (dependencyName === "ava") return "patch"; // Trying to support Node 12.
+        if (dependencyName === "@ava/typescript") return "minor"; // Trying to support Node 12.
+        if (dependencyName === "execa") return "patch"; // Trying to support Node 12.
+        if (dependencyName === "command-line-usage") return "minor"; // Package got big.
+        if (dependencyName === "import-meta-resolve") return "minor"; // For old versions of node.
+        if (major === "0") return "minor";
+        return "latest";
+    },
+};
