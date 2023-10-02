a3c2292b143fd73ddc312b97d957929ba7fbc2c8
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-10-01 19:43:30 -0700

    Node 16

--- .ncurc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    target: (dependencyName, [{ semver, version, operator, major, minor, patch, release, build }]) => {
-        if (dependencyName === "execa") return "patch"; // Trying to support Node 12.
-        if (major === "0") return "minor";
-        return "latest";
-    },
-};
