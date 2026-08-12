c17adea5ff9b64afd6725ad0d7dec810c439efc6
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2026-08-11 18:56:41 -0700

    Switch to TypeScript 7 and Oxlint

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -1,6 +1,5 @@
 module.exports = {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    target: (dependencyName, [{ semver, version, operator, major, minor, patch, release, build }]) => {
+    target: (dependencyName, [{ major }]) => {
         if (dependencyName === "@types/node") return "minor";
         if (major === "0") return "minor";
         return "latest";
